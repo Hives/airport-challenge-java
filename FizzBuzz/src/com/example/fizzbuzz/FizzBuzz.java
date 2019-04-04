95d7e4d3ce19c424ee0cf9f01b5360132f67eca4
Paul Martin <communty.hivemind@gmail.com>
2019-04-04 12:21:24 +0100

    First commit, FizzBuzz.

---
+++ FizzBuzz/src/com/example/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,21 @@
+package com.example.fizzbuzz;
+
+public class FizzBuzz {
+    private static String fizzBuzz(int n) {
+        if (n % 15 == 0) {
+            return "FizzBuzz";
+        } else if (n % 3 == 0) {
+            return "Fizz";
+        } else if (n % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzBuzz(i));
+        }
+    }
+}
