ed88652cda4a3eb74285bf2b0f880a3f0c3498a0
Paul Martin <communty.hivemind@gmail.com>
2019-04-05 08:04:21 +0100

    Better test for landing a plane
    Made airport hanger private, created a public method to check if it
    contains a plane

--- airport-challenge/Airport.java
+++ airport-challenge/Airport.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 
 public class Airport {
-    ArrayList<Plane> hanger = new ArrayList<Plane>();
+    private ArrayList<Plane> hanger = new ArrayList<Plane>();
 
     public ArrayList<Plane> clearForLanding(Plane plane) {
         hanger.add(plane);
         return hanger;
+    }
+
+    public boolean contains(Plane plane) {
+        return hanger.contains(plane);
     }
 }
--- airport-challenge/AirportTest.java
+++ airport-challenge/AirportTest.java
@@ -1,17 +1,18 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.ArrayList;
 
 public class AirportTest {
 
     Airport airport = new Airport();
-    Plane plane = new Plane();
+    Plane plane1 = new Plane();
+    Plane plane2 = new Plane();
+    Plane plane3 = new Plane();
 
     @Test
     public void itCanLandAPlane() {
-        ArrayList<Plane> hanger = airport.clearForLanding(plane);
-        ArrayList<Plane> testHanger = new ArrayList<Plane>();
-        testHanger.add(plane);
-        assertEquals(testHanger, hanger);
+        ArrayList<Plane> hanger = airport.clearForLanding(plane1);
+        assertTrue(airport.contains(plane1));
     }
+
 }
