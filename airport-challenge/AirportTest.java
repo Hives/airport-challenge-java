b6cd58c58457e063eebdc26f892e423540897e74
Paul Martin <communty.hivemind@gmail.com>
2019-04-05 08:10:44 +0100

    User Story 1 implemented
    > As an air traffic controller
    > So I can get passengers to a destination
    > I want to instruct a plane to land at an airport

--- airport-challenge/AirportTest.java
+++ airport-challenge/AirportTest.java
@@ -9,10 +9,23 @@ public class AirportTest {
     Plane plane2 = new Plane();
     Plane plane3 = new Plane();
 
+    // As an air traffic controller 
+    // So I can get passengers to a destination 
+    // I want to instruct a plane to land at an airport
     @Test
-    public void itCanLandAPlane() {
+    public void itCanInstructAPlaneToLand() {
         ArrayList<Plane> hanger = airport.clearForLanding(plane1);
         assertTrue(airport.contains(plane1));
+    }
+
+    @Test
+    public void itCanInstructThreePlanesToLand() {
+        airport.clearForLanding(plane1);
+        airport.clearForLanding(plane2);
+        airport.clearForLanding(plane3);
+        assertTrue(airport.contains(plane1));
+        assertTrue(airport.contains(plane2));
+        assertTrue(airport.contains(plane3));
     }
 
 }
